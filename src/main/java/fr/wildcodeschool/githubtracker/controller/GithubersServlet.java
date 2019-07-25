195d19a86877e076c1a4b69c027d14520ccfe5c2
Eddy LAFOND <edbricole@gmail.com>
2019-07-25 15:26:42 +0200

    Introduction de la couche GithubersService.

--- src/main/java/fr/wildcodeschool/githubtracker/controller/GithubersServlet.java
+++ src/main/java/fr/wildcodeschool/githubtracker/controller/GithubersServlet.java
@@ -1,5 +1,7 @@
 package fr.wildcodeschool.githubtracker.controller;
 
+import fr.wildcodeschool.githubtracker.dao.DumbGithuberDAO;
+import fr.wildcodeschool.githubtracker.dao.GithuberDAO;
 import fr.wildcodeschool.githubtracker.model.Githuber;
 
 import javax.servlet.ServletException;
@@ -9,28 +11,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
+import fr.wildcodeschool.githubtracker.service.GithubersService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @WebServlet(name = "GithubersServlet", urlPatterns = {"/githubers"})
-//@WebServlet(name = "GithubersServlet")
 public class GithubersServlet extends HttpServlet {
 
     private final Logger slf4jLogger = LoggerFactory.getLogger(GithubersServlet.class);
+    private GithubersService githuberServObj=null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        githuberServObj= new GithubersService( );
+        //githuberServObj= new GithubersService(new GithuberDAO());
+    }
+
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ArrayList<Githuber> githubers = new ArrayList<>();
-        githubers.add(new Githuber("Eddy","eddy@gmail", "edylog", "edyId", "https://myavatar.com?img=098"));
-        githubers.add(new Githuber("Valentin","valentin33@gmail", "vallog", "ValId", "https://myavatar.com?img=090"));
-        githubers.add(new Githuber("Alex","alex@gmail", "alexlog", "AlexId", "https://myavatar.com?img=345"));
-        githubers.add(new Githuber("Yannis","yannis@orange.fr", "yalog", "yaId", "https://myavatar.com?img=1235"));
-        githubers.add(new Githuber("Edwin","edwin33@gmail", "Edwinlog", "EdwinId", "https://myavatar.com?img=12"));
-        request.setAttribute("gitlist",githubers);
-        slf4jLogger.info(githubers.get(2).getName().toString());
+        request.setAttribute("gitlist", githuberServObj.getAllGithubers());
+        for (Githuber gt : githuberServObj.getAllGithubers() ) {; }
+            //slf4jLogger.info(githuberServObj.getAllGithubers().get(2).getName().toString());
+        Githuber toto= githuberServObj.getGithuber("edylog");
+        String name= (toto!=null) ? toto.getName().toString() : "Not found";
+        slf4jLogger.info(name);
+
         request.getRequestDispatcher("/githubers.jsp").forward(request,response);
     }
 }
---
+++ src/main/java/fr/wildcodeschool/githubtracker/dao/DumbGithuberDAO.java
@@ -0,0 +1,22 @@
+package fr.wildcodeschool.githubtracker.dao;
+
+import fr.wildcodeschool.githubtracker.model.Githuber;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DumbGithuberDAO implements GithuberDAO{
+    @Override
+    public List<Githuber> getGithubers() {
+        ArrayList<Githuber> githubers = new ArrayList<>();
+        githubers.add(new Githuber("Eddy","eddy@gmail", "edylog", "edyId", "https://myavatar.com?img=098"));
+        githubers.add(new Githuber("Valentin","valentin33@gmail", "vallog", "ValId", "https://myavatar.com?img=090"));
+        githubers.add(new Githuber("Alex","alex@gmail", "alexlog", "AlexId", "https://myavatar.com?img=345"));
+        githubers.add(new Githuber("Yannis","yannis@orange.fr", "yalog", "yaId", "https://myavatar.com?img=1235"));
+        githubers.add(new Githuber("Edwin","edwin33@gmail", "Edwinlog", "EdwinId", "https://myavatar.com?img=12"));
+
+        //return Collections.unmodifiableList(githubers); // Read-only list !
+        return githubers;
+    }
+}
